358fc5fa04d0e90d068ab2f19a50731eb6dabe13
S4mdf0o1 <s4mdf0o1@gmail.com>
2014-09-02 16:05:22 +0200

    stable

--- termlog.sh
+++ termlog.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DESTDIR="/data/backup/termlog"
 FILENAME=`whoami`_`date "+%y%m%d-%H%M"`
-script -t 2> $FILENAME.timing -a $FILENAME.session
+script -t 2> $DESTDIR/$FILENAME.1timing -a $DESTDIR/$FILENAME.2session
 
